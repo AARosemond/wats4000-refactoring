be8ef9d3c720281100d663cfe1c431af14c5cc7d
AARosemond <55867779+AARosemond@users.noreply.github.com>
2020-05-16 16:18:09 -0700

    first of many commits to come

---
+++ src/common/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export const API = axios.create({
+  baseURL: `//api.openweathermap.org/data/2.5/`
+})
+API.interceptors.request.use(function (config) {
+        config.params.APPID = 'f6c081f425905d686971f0b5a280bf98';
+    config.params.units = 'imperial';
+    return config;
+  }, function (error) {
+    return Promise.reject(error);
+  });
